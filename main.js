0b7cbee1a0773aedfd25e7551a308425e0d81353
raghadkamal2001 <raghadkamall2001@gmail.com>
2024-12-26 16:49:47 +0300

    javascript task

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log(-5 * 3)
+console.log("JavaScript" + 50)
+console.log(17 % 5)
+console.log(5 / 10)
+console.log(4 == "4")
+console.log(4 != 5)
+console.log(7 <= 8)
+console.log("hello" + 5)
+let x = 2;
+console.log(Math.ceil(x) - Math.floor(x))
+console.log(Math.pow(x, 2))
+
+let namber = prompt("enter:")
+alert(namber);
+
+let number1 = prompt("enter n1:")
+let number2 = prompt("enter n2:")
+console.log(Number(number1) + Number(number2));
+
+
+let number3 = prompt("enter n3:")
+let number4 = prompt("enter n4:")
+console.log(Number(number3) * Number(number3))
+
+
+let n5 = prompt("enter n5:")
+let n6 = prompt("enter n6:")
+console.log(Number(n5) / Number(n6))
+
+
+let f = 2;
+let c = 6;
+console.log(f + c)
